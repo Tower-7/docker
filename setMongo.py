1102c89fa3fca05efc1c6decd5061a5ebf2f847a
Tower-7 <982155153@qq.com>
2019-03-06 11:46:38 +0800

    v1.0

--- setMongo.py
+++
@@ -1,7 +0,0 @@
-# -*- coding:utf8 -*-
-import os
-# set mongo
-os.system('docker pull mongo')
-os.system('mkdir -p /usr/local/mongo')
-os.system('cd /usr/local/mongo && docker run -p 15015:27017 -v $PWD/db:/data/db -d mongo')
-
