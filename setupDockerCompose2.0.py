a6566fd0362778084c6f2e39a29d60357256d4b6
Tower-7 <982155153@qq.com>
2020-03-17 10:03:20 +0800

    Update setupDockerCompose2.0.py

--- setupDockerCompose2.0.py
+++ setupDockerCompose2.0.py
@@ -1,1 +1,17 @@
 
+# -*- coding:utf8 -*-
+import os
+# 安装docker
+def setupDocker():
+    os.system('sudo yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine')
+    os.system('sudo yum install -y yum-utils device-mapper-persistent-data lvm2')
+    os.system('sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo')
+    os.system('sudo yum install -y docker-ce')
+    os.system('sudo systemctl start docker')
+setupDocker()
+# 安装docker-compose
+def setupCompose():
+    os.system('curl -L https://get.daocloud.io/docker/compose/releases/download/1.25.4/docker-compose-`uname -s-uname -m` > /usr/local/bin/docker-compose')
+    os.system('sudo chmod +x /usr/local/bin/docker-compose')
+    os.system('sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose')
+setupCompose()
